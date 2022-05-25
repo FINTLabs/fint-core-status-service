b4b2a2a8b2e474dde51eec835f11d470052f661c
Frode Sjovatsen <frode@fintlabs.no>
2022-05-25 15:03:32 +0200

    Shit...

--- src/main/java/no/fintlabs/AdapterFullSyncRepository.java
+++ src/main/java/no/fintlabs/AdapterFullSyncRepository.java
@@ -2,12 +2,13 @@ package no.fintlabs;
 
 import no.fintlabs.entities.AdapterFullSyncEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 public interface AdapterFullSyncRepository extends JpaRepository<AdapterFullSyncEntity, String> {
-    Flux<AdapterFullSyncEntity> findAdapterFullSyncEntitiesByOrgIdAndAdapterId(String orgId, String adapterId);
+    List<AdapterFullSyncEntity> findAdapterFullSyncEntitiesByOrgIdAndAdapterId(String orgId, String adapterId);
 
-    Flux<AdapterFullSyncEntity> findAdapterFullSyncEntitiesByOrgIdAndAdapterIdAndCorrId(String orgId, String adapterId, String corrId);
+    List<AdapterFullSyncEntity> findAdapterFullSyncEntitiesByOrgIdAndAdapterIdAndCorrId(String orgId, String adapterId, String corrId);
 
-    Flux<AdapterFullSyncEntity> findAdapterFullSyncEntitiesByOrgIdAndCorrId(String orgId, String corrId);
+    List<AdapterFullSyncEntity> findAdapterFullSyncEntitiesByOrgIdAndCorrId(String orgId, String corrId);
 }
