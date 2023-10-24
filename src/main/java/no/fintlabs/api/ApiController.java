7ed79981599d5bc2c471eab8769b51e8a8c4ca9d
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2023-10-24 13:19:09 +0200

    Add topic method

--- src/main/java/no/fintlabs/api/ApiController.java
+++ src/main/java/no/fintlabs/api/ApiController.java
@@ -1,8 +1,13 @@
 package no.fintlabs.api;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api")
@@ -12,6 +17,11 @@ public class ApiController {
 
     public ApiController(ApiService apiService) {
         this.apiService = apiService;
+    }
+
+    @GetMapping("/topic/{topic}")
+    public ResponseEntity<HashMap<String, HashMap>> getTopic(@PathVariable String topic){
+        return ResponseEntity.ok(apiService.getTopic(topic));
     }
 
 }
--- src/main/java/no/fintlabs/api/ApiService.java
+++ src/main/java/no/fintlabs/api/ApiService.java
@@ -2,6 +2,24 @@ package no.fintlabs.api;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
 @Service
 public class ApiService {
+    public HashMap<String, HashMap> getTopic(String topic) {
+
+        HashMap<String, HashMap> orgIdMap = new HashMap<>();
+        HashMap<String, List<String>> domainMap = new HashMap<>();
+
+        String[] topicParts = topic.split("\\.");
+        String orgId = topicParts[0];
+        String[] domainparts = topicParts[3].split("-");
+        String[] packages = topicParts[3].split("-");
+
+        domainMap.put(domainparts[0], Collections.singletonList(packages[1]));
+        orgIdMap.put(orgId, domainMap);
+        return orgIdMap;
+    }
 }
