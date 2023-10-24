493e31970167627d78d1381235cb888bda687bb1
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2023-10-24 13:03:15 +0200

    Add structure for website backend

---
+++ src/main/java/no/fintlabs/api/ApiController.java
@@ -0,0 +1,17 @@
+package no.fintlabs.api;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    private final ApiService apiService;
+
+    public ApiController(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+}
---
+++ src/main/java/no/fintlabs/api/ApiService.java
@@ -0,0 +1,7 @@
+package no.fintlabs.api;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiService {
+}
