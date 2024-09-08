bb1bd0e34f899dae6eadb1c799e1ac1cd2abd5e3
Hknots <hknots99@gmail.com>
2024-09-08 23:40:30 +0200

    enable schedueling

--- src/main/kotlin/no/fintlabs/Application.kt
+++ src/main/kotlin/no/fintlabs/Application.kt
@@ -2,10 +2,12 @@ package no.fintlabs
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class FintCoreStatusServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<FintCoreStatusServiceApplication>(*args)
+    runApplication<FintCoreStatusServiceApplication>(*args)
 }
