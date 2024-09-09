5c58c5b27a82d55e8086bc7192d2559b4414db1e
Hknots <hknots99@gmail.com>
2024-09-09 07:44:40 +0200

    add logging

--- src/main/kotlin/no/fintlabs/contract/heartbeat/HeartbeatChecker.kt
+++ src/main/kotlin/no/fintlabs/contract/heartbeat/HeartbeatChecker.kt
@@ -1,6 +1,7 @@
 package no.fintlabs.contract.heartbeat
 
 import no.fintlabs.contract.ContractCache
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
@@ -10,12 +11,18 @@ class HeartbeatChecker(
     val contractCache: ContractCache
 ) {
 
+    private val log = LoggerFactory.getLogger(HeartbeatChecker::class.java)
+
     @Scheduled(fixedRateString = "\${fint.heartbeat.check-rate}")
     fun checkHeartbeats() {
+        log.info("Checking heartbeats")
         contractCache.getAll().onEach { contract ->
             heartbeatCache.getLastHeartbeat(contract.adapterId)?.let { lastHeartbeat ->
+                log.info("Last heartbeat: {}", lastHeartbeat)
                 val timeSinceLastHeartbeat = System.currentTimeMillis() - lastHeartbeat
+                log.info("Time since last heartbeat: {}", lastHeartbeat)
                 val expectedIntervalMillis = contract.heartbeatIntervalInMinutes * 60 * 1000
+                log.info("Expected timing: {}", expectedIntervalMillis)
 
                 contract.hasContact = timeSinceLastHeartbeat <= expectedIntervalMillis
                 contractCache.save(contract)
