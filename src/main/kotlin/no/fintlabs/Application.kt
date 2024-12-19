3315a262d9eceb3592fe23068796cefe8e782f8c
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-12-19 12:23:26 +0100

    remove entityscanner

--- src/main/kotlin/no/fintlabs/Application.kt
+++ src/main/kotlin/no/fintlabs/Application.kt
@@ -1,13 +1,11 @@
 package no.fintlabs
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
 @SpringBootApplication
-@EntityScan(basePackages = ["no/fintlabs"])
 class FintCoreStatusServiceApplication
 
 fun main(args: Array<String>) {
