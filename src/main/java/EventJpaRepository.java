b534bc16739bbe00ec32b2e6a714560a039d90b1
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-12-19 12:40:53 +0100

    remove unused files and add dependencie

--- src/main/java/EventJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package no.fintlabs.event;
-
-import jdk.jfr.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventJpaRepository extends JpaRepository<Event, Long> {
-}
