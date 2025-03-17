8fa931c3fdf74856379e82d8b41ee3f386aa7918
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2025-03-17 14:40:18 +0100

    feat: add pageProducer

---
+++ src/main/kotlin/no/fintlabs/page/kafka/PageProducer.kt
@@ -0,0 +1,35 @@
+package no.fintlabs.page.kafka
+
+import no.fintlabs.StatusTopicService
+import no.fintlabs.kafka.event.EventProducer
+import no.fintlabs.kafka.event.EventProducerFactory
+import no.fintlabs.kafka.event.EventProducerRecord
+import no.fintlabs.kafka.event.topic.EventTopicNameParameters
+import no.fintlabs.page.model.PageMetadata
+import java.time.Duration
+
+class PageProducer(
+    facotry: EventProducerFactory,
+    private val statusTopicService: StatusTopicService,
+) {
+    private val pageEventProducer: EventProducer<ResourceEvictionPayload> =
+        facotry.createProducer(ResourceEvictionPayload::class.java)
+
+    fun sendPage(page: PageMetadata) {
+        val topicName = EventTopicNameParameters.builder()
+            .orgId(page.orgId)
+            .eventName("completed-full-sync")
+            .domainContext("fint-core")
+            .build()
+
+        statusTopicService.ensureTopic(topicName, Duration.ofDays(7).toMillis())
+
+        pageEventProducer.send(
+            EventProducerRecord.builder<ResourceEvictionPayload>()
+                .key(page.corrId)
+                .value(ResourceEvictionPayload.fromPageMetadata(page))
+                .topicNameParameters(topicName)
+                .build()
+        )
+    }
+}
