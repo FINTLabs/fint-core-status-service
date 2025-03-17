866bfeee91ce5ad89743c69f13ec5658e493f8b0
Hknots <hknots99@gmail.com>
2025-03-17 11:47:58 +0100

    refactor: encapsulate creating and updating of PageMetadata

--- src/main/kotlin/no/fintlabs/page/model/PageMetaData.kt
+++ src/main/kotlin/no/fintlabs/page/model/PageMetaData.kt
@@ -1,5 +1,6 @@
 package no.fintlabs.page.model
 
+import no.fintlabs.adapter.models.sync.SyncPageMetadata
 import no.fintlabs.adapter.models.sync.SyncType
 
 class PageMetaData(
@@ -14,4 +15,37 @@ class PageMetaData(
     val syncType: SyncType,
     val pages: MutableList<Page> = mutableListOf()
 ) {
+    companion object {
+        fun create(pageMetadata: SyncPageMetadata, syncType: SyncType): PageMetaData {
+            val initialPage = Page(
+                index = pageMetadata.page,
+                pageSize = pageMetadata.pageSize,
+                time = pageMetadata.time
+            )
+            return PageMetaData(
+                corrId = pageMetadata.corrId ?: "",
+                adapterId = pageMetadata.adapterId ?: "",
+                orgId = pageMetadata.orgId ?: "",
+                entityUrl = pageMetadata.uriRef ?: "",
+                totalPages = pageMetadata.totalPages,
+                pagesAcquired = 1,
+                totalEntities = pageMetadata.totalSize,
+                entitiesAquired = pageMetadata.pageSize,
+                syncType = syncType,
+                pages = mutableListOf(initialPage)
+            )
+        }
+
+        fun update(existing: PageMetaData, pageMetadata: SyncPageMetadata): PageMetaData {
+            val newPage = Page(
+                index = pageMetadata.page,
+                pageSize = pageMetadata.pageSize,
+                time = pageMetadata.time
+            )
+            existing.pages.add(newPage)
+            existing.pagesAcquired += 1
+            existing.entitiesAquired += pageMetadata.pageSize
+            return existing
+        }
+    }
 }
