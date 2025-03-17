063ccb380811c03d3cbc8e7060f09b1beeadd10f
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2025-03-17 14:50:05 +0100

    feat: produce page when finished

--- src/main/kotlin/no/fintlabs/page/PageMetadataCache.kt
+++ src/main/kotlin/no/fintlabs/page/PageMetadataCache.kt
@@ -1,20 +1,19 @@
 package no.fintlabs.page
 
 import no.fintlabs.adapter.models.sync.SyncPageMetadata
+import no.fintlabs.contract.ContractCache
+import no.fintlabs.page.kafka.PageProducer
 import no.fintlabs.page.model.PageMetadata
 import org.springframework.stereotype.Component
 import java.util.concurrent.ConcurrentHashMap
 
 @Component
-class PageMetadataCache {
+class PageMetadataCache(
+    private val pageProducer: PageProducer
+) {
 
     private val cache: MutableMap<String, ConcurrentHashMap<String, PageMetadata>> = ConcurrentHashMap()
 
-    fun getAll(): Collection<PageMetadata> =
-        cache.values.flatMap { it.values }
-
-    fun getByOrgId(orgId: String): Collection<PageMetadata> =
-        cache.getOrDefault(orgId, ConcurrentHashMap()).values
 
     fun add(pageMetadata: SyncPageMetadata, syncType: String) {
         requireNotNull(pageMetadata.orgId) { "orgId must be set" }
@@ -22,9 +21,21 @@ class PageMetadataCache {
 
         val orgCache = cache.computeIfAbsent(pageMetadata.orgId) { ConcurrentHashMap() }
         orgCache.compute(pageMetadata.corrId) { _, existing ->
-            existing?.apply { addPage(pageMetadata) }
-                ?: PageMetadata.create(pageMetadata, syncType)
+            existing?.apply {
+                addPage(pageMetadata)
+                if (finished){
+                    pageProducer.sendPage(this)
+                }
+            } ?: run {
+                    val create = PageMetadata.create(pageMetadata, syncType)
+                    if (create.finished){
+                        pageProducer.sendPage(create)
+                    }
+                    create
+                }
         }
     }
+
+
 
 }
