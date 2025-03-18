61fcbdee863fb5787bd31b10ea258ce0dfa08a44
Hknots <hknots99@gmail.com>
2025-03-18 11:13:24 +0100

    refactor: change index to pageNumber

--- src/main/kotlin/no/fintlabs/sync/model/Page.kt
+++ src/main/kotlin/no/fintlabs/sync/model/Page.kt
@@ -3,14 +3,14 @@ package no.fintlabs.sync.model
 import no.fintlabs.adapter.models.sync.SyncPageMetadata
 
 class Page(
-    val index: Long,
+    val pageNumber: Long,
     val pageSize: Long,
     val time: Long
 ) {
     companion object {
         fun fromSyncPageMetadata(syncPageMetadata: SyncPageMetadata): Page {
             return Page(
-                index = syncPageMetadata.page,
+                pageNumber = syncPageMetadata.page,
                 pageSize = syncPageMetadata.pageSize,
                 time = syncPageMetadata.time
             )
