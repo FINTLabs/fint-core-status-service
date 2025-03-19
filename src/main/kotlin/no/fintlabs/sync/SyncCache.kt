7b1a3703fbd745b0c927f54f1bba68eabd5b92a8
Hknots <hknots99@gmail.com>
2025-03-19 10:27:47 +0100

    refactor: remove requireNotNull checks & only focus on cache logic

--- src/main/kotlin/no/fintlabs/sync/SyncCache.kt
+++ src/main/kotlin/no/fintlabs/sync/SyncCache.kt
@@ -1,6 +1,5 @@
 package no.fintlabs.sync
 
-import no.fintlabs.adapter.models.sync.SyncPageMetadata
 import no.fintlabs.sync.kafka.CompletedFullSyncProducer
 import no.fintlabs.sync.model.SyncMetadata
 import org.springframework.stereotype.Component
@@ -20,22 +19,18 @@ class SyncCache(
     fun getByOrgId(orgId: String): Collection<SyncMetadata> =
         cache.getOrDefault(orgId, ConcurrentHashMap()).values
 
-    fun add(syncPageMetdata: SyncPageMetadata, syncType: String) {
-        requireNotNull(syncPageMetdata.orgId) { "orgId must be set" }
-        requireNotNull(syncPageMetdata.corrId) { "corrId must be set" }
-
-        val orgCache = cache.computeIfAbsent(syncPageMetdata.orgId) { ConcurrentHashMap() }
-        orgCache.compute(syncPageMetdata.corrId) { _, existing ->
-            existing?.apply {
-                addPage(syncPageMetdata)
-                processFinishedPage(this)
-            } ?: run {
-                val create = SyncMetadata.create(syncPageMetdata, syncType)
-                processFinishedPage(create)
-                create
+    fun add(sync: SyncMetadata) =
+        cache.getOrPut(sync.orgId) { ConcurrentHashMap() }
+            .compute(sync.corrId) { _, existing ->
+                existing?.apply {
+                    addPage(sync)
+                    processFinishedPage(this)
+                } ?: run {
+                    processFinishedPage(sync)
+                    sync
+                }
             }
-        }
-    }
+
 
     fun processFinishedPage(sync: SyncMetadata) {
         if (sync.finished) {
