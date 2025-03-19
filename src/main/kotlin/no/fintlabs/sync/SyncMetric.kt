eb127690f818189bd24e58d570a00fec90e55f27
Hknots <hknots99@gmail.com>
2025-03-19 16:11:01 +0100

    feat|refactor: add failed syncs & adjust argument type in incrementCompletedSyncs

--- src/main/kotlin/no/fintlabs/sync/SyncMetric.kt
+++ src/main/kotlin/no/fintlabs/sync/SyncMetric.kt
@@ -1,6 +1,7 @@
 package no.fintlabs.sync
 
 import io.micrometer.core.instrument.MeterRegistry
+import no.fintlabs.sync.model.SyncMetadata
 import org.springframework.stereotype.Component
 
 @Component
@@ -8,12 +9,18 @@ class SyncMetric(
     private val meterRegistry: MeterRegistry
 ) {
 
-    fun incrementCompletedSyncs(domain: String, pkg: String, resource: String) {
+    fun incrementCompletedSyncs(sync: SyncMetadata) =
         meterRegistry.counter(
             "core.completed.syncs",
-            "component", "$domain $pkg",
-            "resource", resource
+            "component", "${sync.domain} ${sync.`package`}",
+            "resource", sync.resource
         ).increment()
-    }
+
+    fun incrementFailedSyncs(sync: SyncMetadata) =
+        meterRegistry.counter(
+            "core.failed.syncs",
+            "component", "${sync.domain} ${sync.`package`}",
+            "resource", sync.resource
+        ).increment()
 
 }
