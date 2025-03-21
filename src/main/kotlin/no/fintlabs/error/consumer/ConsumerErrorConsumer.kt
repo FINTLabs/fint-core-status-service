28cf4e093afe74fca834ad5d6b30d76626e3fd24
Hknots <hknots99@gmail.com>
2025-03-21 13:26:29 +0100

    refactor: add ConsumerError to cache

--- src/main/kotlin/no/fintlabs/error/consumer/ConsumerErrorConsumer.kt
+++ src/main/kotlin/no/fintlabs/error/consumer/ConsumerErrorConsumer.kt
@@ -2,20 +2,16 @@ package no.fintlabs.error.consumer
 
 import no.fintlabs.kafka.event.EventConsumerFactoryService
 import no.fintlabs.kafka.event.topic.EventTopicNameParameters
-import no.fintlabs.organisationStat.OrganisatsionStatService
 import no.fintlabs.status.models.error.ConsumerError
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
 import org.springframework.stereotype.Component
 
 @Component
 class ConsumerErrorConsumer(
-    private val organisatsionStatService: OrganisatsionStatService,
+    private val consumerErrorCache: ConsumerErrorCache,
 ) {
-
-    private val logger = LoggerFactory.getLogger(javaClass)
 
     @Bean
     fun registerConsumerErrorConsumer(eventConsumerFactoryService: EventConsumerFactoryService): ConcurrentMessageListenerContainer<String, ConsumerError> {
@@ -33,11 +29,6 @@ class ConsumerErrorConsumer(
 
     fun processEvent(consumerRecord: ConsumerRecord<String, ConsumerError>) {
         val orgId = consumerRecord.topic().split(".")[0]
-        val split = consumerRecord.topic().split("-")
-        val domain = split[split.size - 2]
-        val pkg = split[split.size - 1]
-
-        organisatsionStatService.addError(orgId, "$domain $pkg", consumerRecord.value())
-        logger.info("Consumed error: {}", consumerRecord.value())
+        consumerErrorCache.add(orgId, consumerRecord.value())
     }
 }
