3b704bcd004517cebca7f026ddedb6b841a41bea
Hknots <hknots99@gmail.com>
2025-03-21 13:07:42 +0100

    refactor: rename consumerErrors to errors and convert type to Int

--- src/main/kotlin/no/fintlabs/organisationStat/OrganisastionStat.kt
+++ src/main/kotlin/no/fintlabs/organisationStat/OrganisastionStat.kt
@@ -3,5 +3,5 @@ package no.fintlabs.organisationStat
 class OrganisastionStat(
     val applications: Int = 0,
     val restarts: Int = 0,
-    val consumerErrors: MutableMap<String, List<String>> = mutableMapOf()
+    val errors: Int = 0
 )
