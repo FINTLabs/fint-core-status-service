b8d7449543bda656855350ffaba2d681ee10a7e0
Hknots <hknots99@gmail.com>
2025-03-21 13:41:03 +0100

    fix: increment one error

--- src/main/kotlin/no/fintlabs/organisationStat/OrganisatsionStatService.kt
+++ src/main/kotlin/no/fintlabs/organisationStat/OrganisatsionStatService.kt
@@ -5,16 +5,12 @@ import org.springframework.stereotype.Service
 @Service
 class OrganisatsionStatService(
     private val cache: OrganisationStatCache
-){
+) {
 
-    fun addError(orgId: String, consumer: String, errorMessage: String){
-        cache.get(orgId)?.let {
-            it.consumerErrors.getOrDefault(consumer, mutableListOf<String>().add(errorMessage))
-            cache.add(orgId, it)
-        }?: cache.add(orgId, OrganisastionStat(
-            consumerErrors = mutableMapOf(orgId to listOf(errorMessage))
-        )
-        )
+    fun addError(orgId: String, consumer: String, errorMessage: String) {
+        cache.get(orgId)?.apply {
+            errors += 1
+        } ?: cache.add(orgId, OrganisastionStat(errors = 1))
     }
 
 }
