c3029d3ac9b55a1d0a6f2d3eb376df9496af61ea
Hknots <hknots99@gmail.com>
2025-03-21 13:33:40 +0100

    refactor: rename RequestMapping from errors to error

--- src/main/kotlin/no/fintlabs/error/ErrorController.kt
+++ src/main/kotlin/no/fintlabs/error/ErrorController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/errors")
+@RequestMapping("/error")
 class ErrorController(
     private val prometheusService: PrometheusService
 ) {
