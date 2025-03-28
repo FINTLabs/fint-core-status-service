b4718bfee298c845c71919d57dd956986055e385
Hknots <hknots99@gmail.com>
2025-03-28 10:53:03 +0100

    refactor: create static types & use them in ADAPTER_SYNC_TOPICS

--- src/main/kotlin/no/fintlabs/sync/kafka/KafkaTopicConstants.kt
+++ src/main/kotlin/no/fintlabs/sync/kafka/KafkaTopicConstants.kt
@@ -2,10 +2,14 @@ package no.fintlabs.sync.kafka
 
 class KafkaTopicConstants {
     companion object {
+        val ADAPTER_FULL_SYNC = "adapter-full-sync"
+        val ADAPTER_DELTA_SYNC = "adapter-delta-sync"
+        val ADAPTER_DELETE_SYNC = "adapter-delete-sync"
+
         val ADAPTER_SYNC_TOPICS: Array<String> = arrayOf(
-            "adapter-full-sync",
-            "adapter-delta-sync",
-            "adapter-delete-sync"
+            ADAPTER_FULL_SYNC,
+            ADAPTER_DELTA_SYNC,
+            ADAPTER_DELETE_SYNC
         )
     }
 }
