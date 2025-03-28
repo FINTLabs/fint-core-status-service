fa581f9746ccc5d5476e9d0bf5a1a70fab937fb5
Hknots <hknots99@gmail.com>
2025-03-28 13:42:19 +0100

    refactor: remove duplicate operation

--- src/main/kotlin/no/fintlabs/contract/ContractCache.kt
+++ src/main/kotlin/no/fintlabs/contract/ContractCache.kt
@@ -12,14 +12,4 @@ class ContractCache {
     fun getAll(): MutableCollection<Contract> = cache.values
     fun save(contract: Contract) = cache.put(contract.adapterId, contract)
 
-    fun updateLastActivity(adapterId: String, time: Long) = cache[adapterId]
-        ?.takeIf { time > it.lastActivity }
-        ?.apply { lastActivity = time }
-
-    fun updateLastFullSync(syncPage: SyncPageMetadata) {
-        val (domain, pkg, resource) = syncPage.uriRef?.trim('/')!!.split('/')
-        cache[syncPage.adapterId]?.let {
-            it.getCapability(domain, pkg, resource)!!.updateLastFullSync(syncPage.time)
-        }
-    }
 }
