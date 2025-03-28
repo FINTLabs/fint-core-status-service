28176ce5db6cd89b93f396991af6e185d32a5314
Hknots <hknots99@gmail.com>
2025-03-28 15:22:31 +0100

    feat: increment & decrement absentFullSyncs based on condition

--- src/main/kotlin/no/fintlabs/contract/contractCapabilityScheduler.kt
+++ src/main/kotlin/no/fintlabs/contract/contractCapabilityScheduler.kt
@@ -1,22 +1,32 @@
 package no.fintlabs.contract
 
+import no.fintlabs.sync.SyncMetricService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
 class ContractCapabilityScheduler(
-    private val contractCache: ContractCache
+    private val contractCache: ContractCache,
+    private val syncMetricService: SyncMetricService
 ) {
 
     @Scheduled(cron = "0 * * * * *")
     fun updateFollowsContract() {
         contractCache.getAll().onEach { contract ->
             contract.capabilities.values.forEach { capability ->
+                val oldFollowsContract = capability.followsContract
                 capability.updateFollowsContract()
-                if (capability.followsContract.not()) {
-
-                }
+                compareFollowsContract(oldFollowsContract, capability.followsContract)
             }
         }
     }
+
+    fun compareFollowsContract(old: Boolean, new: Boolean) {
+        if (!old && new) {
+            syncMetricService.decrementAbsentFullsyncs()
+        } else if (old == !new){
+            syncMetricService.incrementAbsentFullsyncs()
+        }
+    }
+
 }
