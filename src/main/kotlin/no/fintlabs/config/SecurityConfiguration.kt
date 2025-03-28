1d1d941c064446001fccd1451ae71043ef754d37
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2025-03-28 12:50:39 +0100

    update securityconfig

---
+++ src/main/kotlin/no/fintlabs/config/SecurityConfiguration.kt
@@ -0,0 +1,23 @@
+package no.fintlabs.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfiguration {
+
+    @Bean
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
+        http
+            .authorizeExchange { exchanges ->
+                exchanges.pathMatchers("/actuator/prometheus").permitAll()
+                    .anyExchange().authenticated()
+            }
+            .oauth2ResourceServer { it.jwt(Customizer.withDefaults()) }
+            .build()
+}
