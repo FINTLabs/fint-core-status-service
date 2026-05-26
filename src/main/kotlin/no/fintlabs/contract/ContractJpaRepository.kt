38c900d05c47af0680710be47b2781bf6c68e929
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2026-05-26 15:08:44 +0200

    fix query

--- src/main/kotlin/no/fintlabs/contract/ContractJpaRepository.kt
+++ src/main/kotlin/no/fintlabs/contract/ContractJpaRepository.kt
@@ -2,12 +2,15 @@ package no.fintlabs.contract
 
 import no.fintlabs.contract.model.ContractEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ContractJpaRepository : JpaRepository<ContractEntity, String> {
 
     fun getByOrgId(orgid: String): MutableList<ContractEntity>?
+
+    @Query("SELECT c FROM ContractEntity c JOIN c.capabilities cap WHERE c.orgId = :orgId AND cap.componentName = :componentName")
     fun findByOrgIdAndCapabilitiesComponentName(orgId: String, componentName: String): List<ContractEntity>
 
 }
