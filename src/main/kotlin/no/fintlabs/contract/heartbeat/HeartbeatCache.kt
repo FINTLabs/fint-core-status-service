bca40ce0243ac2de61696c0ab28cbff2e47d247e
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2026-05-28 14:41:43 +0200

    use current timestamp

--- src/main/kotlin/no/fintlabs/contract/heartbeat/HeartbeatCache.kt
+++ src/main/kotlin/no/fintlabs/contract/heartbeat/HeartbeatCache.kt
@@ -8,7 +8,7 @@ class HeartbeatCache {
 
     private val cache: MutableMap<String, Long> = mutableMapOf()
 
-    fun add(adapterHeartbeat: AdapterHeartbeat) = cache.put(adapterHeartbeat.adapterId, adapterHeartbeat.time)
+    fun add(adapterHeartbeat: AdapterHeartbeat) = cache.put(adapterHeartbeat.adapterId, System.currentTimeMillis())
 
     fun getLastHeartbeat(adapterId: String) = cache[adapterId]
 
